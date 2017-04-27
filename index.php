629b76cab0b8cbb09c98f4209a3b8e9e459c4a5a
input image inc <philipbartom@gmail.com>
2017-04-27 14:56:07 -0230

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 		$email = $_POST['email'];
 		$message = $_POST['message'];
 		$human = intval($_POST['human']);
-		$from = 'Demo Contact Form'; 
-		$to = 'example@domain.com'; 
+		$from = 'Input Image Website'; 
+		$to = 'philipbarton815@gmail.com'; 
 		$subject = 'Message from Contact Demo ';
 		
 		$body ="From: $name\n E-Mail: $email\n Message:\n $message";
